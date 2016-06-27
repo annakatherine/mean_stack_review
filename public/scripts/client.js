47eb87d6fad0fd509289e1cc707e25bfdc431ccd
anna baker <annakatherinebaker@gmail.com>
2016-06-27 11:11:48 -0500

    more stuff

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,11 @@
 console.log( 'js sourced' );
+
+myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'mondayController', ['$scope', '$http', function( $scope, $http ){
+//test get user input
+$scope.whatAreWeTryingToDo = function(){
+  console.log( 'in whatAreWeTryingToDo');
+};
+
+}]);//end of mondayController
